bae9a773ef729d25a3a55afb5e95a1fd32eb8fdd
soniatwum <soniaette@yahoo.com>
2023-10-06 21:19:10 -0700

    Lowercase and uppercase alphabets.

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+/**
+ *  main - Printing the alphabet in both lowercase and upper case.
+ *  Return: On success. 0.
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	putchar(letter);
+
+	for (letter = 'A'; letter <= 'Z'; letter++)
+	putchar(letter);
+	putchar('\n');
+	return (0);
+}

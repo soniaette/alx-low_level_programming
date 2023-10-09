c3180e5293894b52620c3dd3b8d175f0207baa57
soniatwum <soniaette@yahoo.com>
2023-10-09 15:55:03 -0700

    Printing all possible different combinations of three digits.

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+
+/**
+ * main - Printing all possible different combinations of three digits
+ * in ascending order separated by comma and followed by space.
+ *
+ * Return: On success 0.
+ *
+ */
+
+int main(void)
+{
+	int num1, num2, num3;
+
+	for (num1 = 0; num1 < 8; num1++)
+	{
+		for (num2 = num1 + 1; num2 < 9; num2++)
+		{
+			for (num3 = num2 + 1; num3 < 10; num3++)
+			{
+				putchar((num1 % 10) + '0');
+				putchar((num2 % 10) + '0');
+				putchar((num3 % 10) + '0');
+
+				if (num1 == 7 && num2 == 8 && num3 == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+
+			}
+		}
+	}
+
+		putchar('\n');
+
+		return (0);
+
+}
